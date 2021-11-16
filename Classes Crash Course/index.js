51e4552c1917bffb1e2927b6a324171227c895db
SEN-SENSATION <prawich.th@gmail.com>
2021-11-16 15:45:47 +0700

    Classes Crash Course

---
+++ Classes Crash Course/index.js
@@ -0,0 +1,13 @@
+class Regtangle {
+  // Setup an object
+  constructor() {
+    console.log("The regtangle is being created");
+
+    this.width = 5;
+    this.color = "white";
+    this.height = 3;
+  }
+}
+
+let myRegtangle1 = new Regtangle();
+let myRegtangle2 = new Regtangle();
